f222502c645640432fe2b43e025a5bab1a5fbe1c
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-02-09 10:11:19 -0300

    new commit

--- src/patterns/projectsCard/projectsCard.js
+++ src/patterns/projectsCard/projectsCard.js
@@ -5,9 +5,9 @@ export function ProjectCard(props) {
         <div className="bg-yelloPrimary p-2">
             <a className={props.className,``} href={props.href}>
                 <img src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg"/>
-                <h3 className="text-white text-2xl font-bold">{props.title}</h3>
+                <h3 className="text-white text-2xl font-bold font-h2Title">{props.title}</h3>
             </a>
-            <p>{props.text}</p>
+            <p className="text-sm bg-white p-2 font-body">{props.text}</p>
         </div>
  )   
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/patterns/**/*.{js,ts,jsx,tsx}'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
