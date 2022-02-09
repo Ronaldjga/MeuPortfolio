2ed13557669778664673075faed8aa66565d3275
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-02-09 18:23:58 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/patterns/**/*.{js,ts,jsx,tsx}'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    './src/patterns/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
